28ebdc686695cc0946e63451f5e59d231d29fda8
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-01-10 13:15:59 -0500

    first commit. Building a restSharp console app that will pull from a cryptocurrency API.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var apiCallTask = ApiHelper.ApiCall("WcATy9IyxwbTeHbB0lsYcTqr6bAeQEmV");
+            var result = apiCallTask.Result;
+            Console.WriteLine(result);
+        }
+    }
+
+    class ApiHelper
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient client = new RestClient("https://blockchain.info/ticker");
+            RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
